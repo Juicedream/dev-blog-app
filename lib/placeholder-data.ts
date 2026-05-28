47104f65ce4612a3abe545c5dd43ed18b52baac3
Juicedream <114106555+Juicedream@users.noreply.github.com>
2026-05-28 11:38:05 +0100

    Fix

---
+++ lib/placeholder-data.ts
@@ -0,0 +1,14 @@
+export const users = [
+  {
+    name: "Jude Nwadiogor",
+    email: "judexfrayo@gmail.com",
+    password: "12345678",
+    role: "admin",
+  },
+  {
+    name: "James Allen",
+    email: "james.allen@gmail.com",
+    password: "12345678",
+    role: "user",
+  },
+];
